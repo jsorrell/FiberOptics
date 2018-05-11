48935768e581741410de7af78962512891a216fd
Jack Sorrell <Jack@jacksorrell.com>
2018-05-11 17:01:06 -0400

    Initial Commit

---
+++ src/main/java/com/jsorrell/fiberoptics/FiberOptics.java
@@ -0,0 +1,10 @@
+package com.jsorrell.fiberoptics;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = FiberOptics.MODID, name = FiberOptics.NAME, version = FiberOptics.VERSION, useMetadata = true)
+public class FiberOptics {
+    public static final String MODID = "fiberoptics";
+    public static final String NAME = "Fiber Optics";
+    public static final String VERSION = "1.0";
+}
